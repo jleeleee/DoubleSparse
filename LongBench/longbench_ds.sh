ea7adbad5eed8e3f642f125b188f88ac1aae8354
Jihyeong Lee <matt.j.lee98@gmail.com>
2024-12-01 12:47:14 -0500

    a

--- LongBench/longbench_ds.sh
+++ LongBench/longbench_ds.sh
@@ -1,13 +1,19 @@
 model="llama3.1-8b-instruct-128k"
 
+
 for task in "musique" "narrativeqa" "qasper"
 do
-
-    for budget in 64 128 # 256 512 1024 2048 4096
+    for group_factor in 2 4 8
     do
-        python -u pred.py \
-            --model $model --task $task \
-            --ds --heavy_const $budget --group_factor 2 --q_bits 2 --channel q
+        for q_bits in 2 4 8
+        do
+            for budget in 64 128 256 512 # 1024 2048 4096
+            do
+                python -u pred.py \
+                    --model $model --task $task \
+                    --ds --heavy_const $budget --group_factor 2 --q_bits 2 --channel q
+            done
+        done
     done
 done
 
--- LongBench/longbench_quest.sh
+++ LongBench/longbench_quest.sh
@@ -5,7 +5,7 @@ do
     python -u pred.py \
         --model $model --task $task
 
-    for budget in 256 512 1024 2048 4096
+    for budget in 256 512 1024 2048 # 4096
     do
         CUDA_VISIBLE_DEVICES=2 python -u pred.py \
             --model $model --task $task \
