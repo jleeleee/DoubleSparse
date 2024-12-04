fc2bd0ef651312598290cae4677c3dfc3f441448
Jihyeong Lee <matt.j.lee98@gmail.com>
2024-12-04 04:06:07 +0000

    Additional experiments

--- LongBench/longbench_ds.sh
+++ LongBench/longbench_ds.sh
@@ -3,15 +3,15 @@ model="llama3.1-8b-instruct-128k"
 
 for task in "musique" "narrativeqa" "qasper"
 do
-    for group_factor in 2 4 8
+    for budget in 64 128 256 512 # 1024 2048 4096
     do
-        for q_bits in 2 4 8
+        for q_bits in 4 8
         do
-            for budget in 64 128 256 512 # 1024 2048 4096
+            for group_factor in 4 8
             do
                 python -u pred.py \
                     --model $model --task $task \
-                    --ds --heavy_const $budget --group_factor 2 --q_bits 2 --channel q
+                    --ds --heavy_const $budget --group_factor $group_factor --q_bits $q_bits --channel q
             done
         done
     done
